34b2d886bbcabc72d9469d1e8c5a7ad500d1c974
Eby Stewart <ebyystewart@gmail.com>
2025-05-06 09:21:50 +0530

    Tested loading crc module- concluded no app space changes needed

--- LDD_LoadModule/LoadModule.c
+++ LDD_LoadModule/LoadModule.c
@@ -2,8 +2,11 @@
 #include <linux/init.h>
 #include <linux/stat.h>
 #include <linux/kmod.h>
-/* Ref #1:  https://www.oreilly.com/library/view/linux-device-drivers/0596000081/ch11.html */
+#include <linux/capability.h>
+
+/* Ref #1: https://www.oreilly.com/library/view/linux-device-drivers/0596000081/ch11.html */
 /* Ref #2: https://duasynt.com/blog/linux-kernel-module-autoloading */
+/* ref #3: https://access.redhat.com/solutions/4246821 */
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Eby Stewart");
@@ -14,7 +17,21 @@ void __exit ModuleExit(void);
 
 int __init ModuleInit(void){
     printk("LKM Initialized\n");
-    request_module("kthread");
+
+    struct module *mod;
+    struct mutex module_mutex;
+    mutex_lock(&module_mutex);
+    //mod = find_module("kthread"); // need to check find_module usage
+    //if(!mod)
+    {
+        mutex_unlock(&module_mutex);
+        request_module("cmac"); // This loads cmac from /lib/modules/6.11.0-24-generic/kernel/crypto
+    }
+    //else
+    {
+        try_module_get(mod);
+        //mutex_unlock(&module_mutex);
+    }
     return 0;
 }
 
--- README.md
+++ README.md
@@ -9,3 +9,7 @@ usage example: sudo insmod ModuleParam.ko dev_no=8 dev_name="cake"
 LDD_Inter-Module_Communication: uses symbol_get()/symbol_put(), inter_module_get()/inter_module_put()/inter_module_register(), inter_module_unregister() APIs to demonstrate communication between two kernel modules.
 
 LDD_LoadModule: Demonstrates loading of another kernel module using request_module() API
+usage:
+    #1: The module to be loaded should be already available as an installed module in /lib/modules/<kernel_version>/kernel/../
+    #2: The list of loaded modules can be found using lsmod command
+    #3: The list of installed modules can be found by "cat /lib/modules/<kernel_version>/modules.dep"
